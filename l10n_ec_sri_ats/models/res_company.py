281ee232092c5f409540e18b491171b8ab9936ca
Daniel Mendieta <desarrollo@libre.ec>
2015-11-23 17:14:59 -0500

    [FIX] Fix PEP8 / Arregla errores de PEP8

--- l10n_ec_sri_ats/models/res_company.py
+++ l10n_ec_sri_ats/models/res_company.py
@@ -5,7 +5,7 @@ from openerp import models, fields
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    establecimientos = fields.Char(string='Establecimientos activos',
+    establecimientos = fields.Char(
+        string='Establecimientos activos', size=3,
         help="""Ingrese el número de establecimientos activos
-                inscritos en el R.U.C.""",
-        size=3)
+                inscritos en el R.U.C.""")
