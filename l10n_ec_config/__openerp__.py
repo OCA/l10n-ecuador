e78de72363fc650e8dc1d2b79d05a1836fdeea16
Daniel Mendieta <damendieta@gmail.com>
2016-11-18 09:33:38 -0500

    [NEW MODULE] Configuration of the Ecuadorian localization modules

---
+++ l10n_ec_config/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Ecuador Easy Install',
+    'version': '9.0.1.0.0',
+    'summary': "Ecuador's localization config settings",
+    'author': "Fabrica de Software Libre,Odoo Community Association (OCA)",
+    'website': "www.libre.ec",
+    'category': 'Tools',
+    'images': [
+    ],
+    'depends': [
+        'base_setup'
+    ],
+    'data': [
+        'views/res_config.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
