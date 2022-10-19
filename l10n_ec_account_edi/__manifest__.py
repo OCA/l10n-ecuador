cf74bed6058d4754a2391ffa60dad15380d1ce92
Carlos Lopez <celm1990@gmail.com>
2022-10-18 22:10:06 -0500

    [ADD] l10n_ec_account_edi: estructuras iniciales necesarias para EDI * primeras pruebas para generacion, firma, envio de xml al SRI con facturas de cliente * archivos xsd para validar estructura de xml antes de enviar al SRI

---
+++ l10n_ec_account_edi/__manifest__.py
@@ -0,0 +1,35 @@
+{
+    "name": "Electronic Ecuadorian Localization",
+    "summary": "Electronic data interchange adapted Ecuadorian localization",
+    "category": "Account",
+    "author": "Odoo Community Association (OCA), "
+    "Renan Nazate, Gabriel, Leonardo, Yazber Romero",
+    "website": "https://github.com/OCA/l10n-ecuador",
+    "license": "AGPL-3",
+    "version": "15.0.1.0.0",
+    "depends": ["account", "account_edi", "l10n_ec", "l10n_ec_base"],
+    "external_dependencies": {
+        "python": ["cryptography", "xmlsig==0.1.9", "xades==0.2.4", "zeep"]
+    },
+    "data": [
+        "security/ir.model.access.csv",
+        "data/edi_format_data.xml",
+        "data/edi_templates/edi_info_tributaria_data.xml",
+        "data/edi_templates/edi_invoice.xml",
+        "report/edi_report_templates.xml",
+        "report/report_edi_invoice.xml",
+        "report/report_invoice.xml",
+        "views/sri_key_type_view.xml",
+        "views/menu_root.xml",
+        "views/account_move_view.xml",
+        "views/res_config_settings_view.xml",
+        "views/account_edi_document_view.xml",
+    ],
+    "assets": {
+        "web.report_assets_common": [
+            "l10n_ec_account_edi/static/src/scss/report_layout.scss"
+        ],
+    },
+    "installable": True,
+    "auto_install": False,
+}
---
+++ l10n_ec_account_edi/models/res_config_settings.py
@@ -0,0 +1,17 @@
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    l10n_ec_type_environment = fields.Selection(
+        related="company_id.l10n_ec_type_environment", readonly=False
+    )
+    l10n_ec_key_type_id = fields.Many2one(
+        comodel_name="sri.key.type",
+        related="company_id.l10n_ec_key_type_id",
+        readonly=False,
+    )
+    l10n_ec_invoice_version = fields.Selection(
+        related="company_id.l10n_ec_invoice_version", readonly=False
+    )
