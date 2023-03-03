0cdb7fdb7ac73f9e0b35076c11798a923da4c51a
Carlos Lopez <celm1990@gmail.com>
2023-03-02 21:04:13 -0500

    [IMP] l10n_ec_base: no permitir eliminar/archivar/modificar consumidor final Por temas de seguridad solo se permite al admin de sistemas hacer cambios en el consumidor final

--- l10n_ec_base/models/res_partner.py
+++ l10n_ec_base/models/res_partner.py
@@ -1,4 +1,5 @@
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class ResPartner(models.Model):
@@ -21,3 +22,24 @@ class ResPartner(models.Model):
             and (_vat[2] != "9" or len(_vat) != 13 or _vat[-3:] == "000")
         ):
             return super(ResPartner, self).check_vat()
+
+    def write(self, values):
+        for partner in self:
+            if (
+                partner.vat in ["9999999999", "9999999999999"]
+                and not self.env.is_system()
+                and (
+                    "name" in values
+                    or "vat" in values
+                    or "active" in values
+                    or "country_id" in values
+                )
+            ):
+                raise UserError(_("You cannot modify record of final consumer"))
+        return super(ResPartner, self).write(values)
+
+    def unlink(self):
+        for partner in self:
+            if partner.vat in ["9999999999", "9999999999999"]:
+                raise UserError(_("You cannot unlink final consumer"))
+        return super(ResPartner, self).unlink()
