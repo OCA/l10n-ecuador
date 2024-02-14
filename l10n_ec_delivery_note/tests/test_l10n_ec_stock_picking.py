db7bd1d6266ba9404d8a32a4f056b35a5395d44f
Jorge Luis <jorjoluiso@gmail.com>
2024-02-14 16:43:59 -0500

    [FIX] test_l10n_ec_picking_stock_lots

--- l10n_ec_delivery_note/tests/test_l10n_ec_stock_picking.py
+++ l10n_ec_delivery_note/tests/test_l10n_ec_stock_picking.py
@@ -83,20 +83,17 @@ class TestL10nStockPicking(TestL10nDeliveryNoteCommon):
     def test_l10n_ec_picking_stock_lots(self):
         """Transferencia y guía con trazabilidad por lotes"""
         self.setup_edi_delivery_note()
-        # lot = self.setup_stock_traceability()
+        lot = self.setup_stock_traceability()
         picking = self._l10n_ec_create_or_modify_picking(quantity=1)
         picking.action_confirm()
         # Validar sin escoger el lote
         with self.assertRaises(UserError):
             picking.move_ids_without_package.quantity = 1
             picking.button_validate()
-        move_form = Form(
-            picking.move_ids_without_package, view="stock.view_stock_move_operations"
-        )
-        with move_form.move_line_ids.new() as line:
-            # line.lot_id = lot
-            line.quantity = 1
-        move_form.save()
+
+        picking.move_line_ids.quantity = 1
+        picking.move_line_ids.lot_id = lot
+
         picking_context = picking.button_validate()
         wiz = Form(
             self.env[picking_context["res_model"]].with_context(
