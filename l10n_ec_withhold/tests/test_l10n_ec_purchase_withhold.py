1816330edd0ef47cf90ba56069051e3704749dde
Leonardo <gomezgleonardob@riseup.net>
2026-04-11 11:03:04 -0500

    Fix:Test

--- l10n_ec_withhold/tests/test_l10n_ec_purchase_withhold.py
+++ l10n_ec_withhold/tests/test_l10n_ec_purchase_withhold.py
@@ -96,8 +96,7 @@ class TestL10nPurchaseWithhold(TestL10nECEdiCommon):
         invoice = self._l10n_ec_create_in_invoice(self.partner_ruc, auto_post=True)
         self.assertFalse(invoice.l10n_ec_withhold_active)
         msj_expected = _(
-            "Please select only invoice "
-            "what satisfies the requirements for create withhold"
+            "Please select only invoice , what satisfies the requirements for create withhold"
         )
         with self.assertRaisesRegex(UserError, msj_expected):
             invoice.action_try_create_ecuadorian_withhold()
